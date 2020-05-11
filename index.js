f4fb47b3299235e53cc671ca9e207fa8a88779ca
Sahar Derbel <derbel.sahar@insat.u-carthage.tn>
2020-05-11 09:41:43 +0100

    simple node module

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-var rect = {
-    perimeter: (x,y) => (2*(x+y)),
-    area: (x,y) => (x*y)
-};
-
+var rect = require('./rectangle');
 function solveRect(x,y)  {
     console.log("solving for rect");
     if(x <=0 || y <= 0) {
